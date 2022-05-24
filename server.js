137222b33e220b60be2ac9c7d873cc25a781d553
Kenzohfs <88584837+Kenzohfs@users.noreply.github.com>
2022-05-23 21:03:07 -0300

    update backtick

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.delete('/api/usuarios/:id', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log("Example app listening at http://localhost:${port}");
+    console.log(`Example app listening at http://localhost:${port}`);
 })
 
 //GET - Buscar informações // req.query
